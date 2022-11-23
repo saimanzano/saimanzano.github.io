30794f8cef68883c5aa35ea8dae47a6bb3f8a2ff
Saioa Manzano <saioa.manzano@bsc.es>
2022-11-23 14:19:01 +0100

    Update org-locations.js

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,22 +1,13 @@
 var addressPoints = [
   [
-    "Berkeley CA, USA",
-    37.8708393,
-    -122.2728638
+    "Sitges, Spain", 
+    41.2363907211,
+    1.7750302332
   ],
   [
-    "London, UK",
-    51.5073219,
-    -0.1276473
-  ],
-  [
-    "San Francisco, California",
-    37.7792808,
-    -122.4192362
-  ],
-  [
-    "Los Angeles, CA",
-    34.0543942,
-    -118.2439408
+    "Sant Feliu de Guíxols, Spain",
+    41.78333,
+    3.03333
   ]
+
 ];
