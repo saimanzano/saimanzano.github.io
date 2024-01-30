f6319fcc768c675a54b6afc91583399daf9acd11
Saioa Manzano-Morales <saioa.manzano@bsc.es>
2024-01-30 20:34:29 +0100

    Create map.R

---
+++ map.R
@@ -0,0 +1,12 @@
+library(maps); library(rbokeh)
+table <- read.csv("../_data/input.csv")
+latlon <- geocode_OSM(table$City, details = FALSE, as.data.frame = TRUE)
+table$lat <- latlon$lat
+table$long <- latlon$lon
+
+plot <- suppressWarnings(figure(width = 800, height = 450, padding_factor = 0) %>%
+  ly_map("world", col = "gray") %>%
+  ly_points(long, lat, data = table, size = 5,
+            hover = c(Name, Year, Type), color = Type))
+widgetframe::frameWidget(plot,width=600,height=400)
+htmlwidgets::saveWidget(plot, "../talkmap/map-plotly.html")
