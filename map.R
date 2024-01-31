e2b96ea4c75835b518e7989c75c0497145f0903f
Saioa Manzano-Morales <saioa.manzano@bsc.es>
2024-01-31 09:12:31 +0100

    Update map.R

--- map.R
+++ map.R
@@ -1,12 +1,12 @@
 library(maps); library(rbokeh)
-table <- read.csv("../_data/input.csv")
+table <- read.csv("input.csv")
 latlon <- geocode_OSM(table$City, details = FALSE, as.data.frame = TRUE)
 table$lat <- latlon$lat
 table$long <- latlon$lon
 
-plot <- suppressWarnings(figure(width = 800, height = 450, padding_factor = 0) %>%
-  ly_map("world", col = "gray") %>%
+plot <- suppressWarnings(figure(width = 800, height = 450, padding_factor = 0) %>% 
+  ly_map("world", col = "gray") %>% x_range(c(-20,45)) %>% y_range(c(30,80))  %>% 
   ly_points(long, lat, data = table, size = 5,
             hover = c(Name, Year, Type), color = Type))
 widgetframe::frameWidget(plot,width=600,height=400)
-htmlwidgets::saveWidget(plot, "../talkmap/map-plotly.html")
+htmlwidgets::saveWidget(plot, "map.html")
